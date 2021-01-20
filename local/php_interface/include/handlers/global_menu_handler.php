c0d5505302017f552d303cd28e3131f9398bac47
Hoqi <dmitriy.davydov166@gmail.com>
2021-01-20 13:26:47 +0300

    [ex2-95] fix code

--- local/php_interface/include/handlers/global_menu_handler.php
+++ local/php_interface/include/handlers/global_menu_handler.php
@@ -4,14 +4,14 @@ class GlobalMenuHandler {
 
     function onGlobalMenu(&$aGlobalMenu, &$aModuleMenu){
        global $USER;
-       foreach ($aGlobalMenu as $key => $value){
-           if ($key !== 'global_menu_content'){
-               unset($aGlobalMenu[$key]);
-           }
-       }
+    
        $arGroups = $USER->GetUserGroupArray();
        if (in_array(CONTENT_MANAGER_GROUP_ID,$arGroups) && !in_array(ADMIN_GROUP_ID,$arGroups)){
-           unset($aGlobalMenu['global_menu_desktop']);
+        foreach ($aGlobalMenu as $key => $value){
+            if ($key !== 'global_menu_content'){
+                unset($aGlobalMenu[$key]);
+            }
+        }
            foreach ($aModuleMenu as $key => $value){
                if ($aModuleMenu[$key]['parent_menu'] != 'global_menu_content' ||
                    $aModuleMenu[$key]['items_id'] == 'menu_iblock'
