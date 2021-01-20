f619d6cf3fb8eb4caee0fd7c159d506590befd47
Hoqi <dmitriy.davydov166@gmail.com>
2021-01-20 13:56:49 +0300

    [ex2-94] super SEO tool, update onEpilog

--- local/php_interface/include/const.php
+++ local/php_interface/include/const.php
@@ -1,5 +1,6 @@
 <?
 define('PRODUCT_IBLOCK_ID',2);
+define('META_IBLOCK_ID',6);
 
 define('CONTENT_MANAGER_GROUP_ID',5);
 define('ADMIN_GROUP_ID',1);
--- local/php_interface/include/handlers/client_error_handler.php
+++ local/php_interface/include/handlers/client_error_handler.php
@@ -1,17 +1,40 @@
 <?
 AddEventHandler("main","OnEpilog",
-Array("ClientErrorHandler", "onNotFound"));
+Array("ClientErrorHandler", "onEpilog"));
 
 class ClientErrorHandler {
-    function onNotFound(){      
+    function onEpilog(){
+        global $APPLICATION;      
         if (defined("ERROR_404") && ERROR_404 === "Y") {          
-            global $APPLICATION;
             CEventLog::Add(array(
                 "SEVERITY" => "INFO",
                 "AUDIT_TYPE_ID" => "ERROR_404",
                 "MODULE_ID" => "main",
                 "DESCRIPTION" => $APPLICATION->GetCurUri(),
             ));
+            return;
+        }
+        $page = $APPLICATION->GetCurPage();
+        $arSort = false;
+        $arFilter = array(
+            "IBLOCK_ID" => META_IBLOCK_ID,
+            "ACTIVE" => "Y",
+            "NAME" => $page,
+        );
+        $arGroupBy = false;
+        $arNavStartParams = array("nTopCount" => false);
+        $arSelect = array("ID","NAME","PROPERTY_TITLE","PROPERTY_DESCRIPTION");
+        $cib = new CIBlockElement;
+        $BDres = $cib->GetList(
+            $arSort,
+            $arFilter,
+            $arGroupBy,
+            $arNavStartParams,
+            $arSelect,
+        );
+        if ($res = $BDres->GetNext()){
+            $APPLICATION->SetPageProperty('title',$res["PROPERTY_TITLE_VALUE"]);
+            $APPLICATION->SetPageProperty('description',$res["PROPERTY_DESCRIPTION_VALUE"]);
         }
     }
 }
