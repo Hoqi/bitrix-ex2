03eac9742a4f90f5b7d5f2164c8a7ba7a9496913
Hoqi <dmitriy.davydov166@gmail.com>
2021-01-28 11:56:55 +0300

    [ex2-32] rename variable and added array check

--- local/templates/furniture_pale-blue/components/bitrix/news/.default/bitrix/news.list/.default/component_epilog.php
+++ local/templates/furniture_pale-blue/components/bitrix/news/.default/bitrix/news.list/.default/component_epilog.php
@@ -1,7 +1,6 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
-//var_dump($arResult);
+
 if (isset($arResult["SPECIAL_DATE"])){
-    //var_dump($arResult);
     $APPLICATION->SetPageProperty("specialdate",$arResult["SPECIAL_DATE"]);
 }
 ?>
--- local/templates/furniture_pale-blue/components/bitrix/news/.default/bitrix/news.list/.default/result_modifier.php
+++ local/templates/furniture_pale-blue/components/bitrix/news/.default/bitrix/news.list/.default/result_modifier.php
@@ -1,7 +1,10 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 if (isset($arParams["SHOW_DATE"])){
-    $tmp = $this->__component;
-    $arResult["SPECIAL_DATE"] = $arResult["ITEMS"][0]["ACTIVE_FROM"];
-    $tmp->SetResultCacheKeys(array("SPECIAL_DATE"));
+    $component = $this->__component;
+
+    if (isset($arResult["ITEMS"][0]["ACTIVE_FROM"]){
+        $arResult["SPECIAL_DATE"] = $arResult["ITEMS"][0]["ACTIVE_FROM"];
+        $component->SetResultCacheKeys(array("SPECIAL_DATE"));
+    }
 }
 ?>
